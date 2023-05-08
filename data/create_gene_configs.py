4c7d76754d2a3b4fb8dcd9b6dcc33955b6042e76
danielsf <scott.daniel@alleninstitute.org>
2023-05-08 15:18:20 -0700

    "productionize" creation of gene config

--- data/create_gene_configs.py
+++ data/create_gene_configs.py
@@ -1,16 +1,44 @@
+import argparse
 import json
+import pathlib
 
-config = []
-with open("raw/extracted_wb10x_gene_identifiers.csv", "r") as in_file:
-    header = in_file.readline().strip().split(',')
-    header_to_idx = {
-       n:ii for ii, n in enumerate(header)}
 
-    for line in in_file:
-        params = line.strip().split(',')
-        this = {
-            'gene_identifier': params[header_to_idx['gene_identifer']],
-            'gene_symbol': params[header_to_idx['gene_symbol']]}
-        config.append(this)
-with open('configs/gene_config.json', 'w') as out_file:
-    out_file.write(json.dumps(config, indent=2))
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--src_path',
+        type=str,
+        default=None,
+        help='path to .csv with cell metadata')
+    parser.add_argument(
+        '--dst_path',
+        type=str,
+        default=None,
+        help='path to file to be written')
+    parser.add_argument(
+        '--column_names',
+        type=str,
+        default=None,
+        nargs='+',
+        help='columns to keep in obs')
+    args = parser.parse_args()
+
+    config = []
+    with open(args.src_path, "r") as in_file:
+        header = in_file.readline().strip().split(',')
+        header_to_idx = {
+           n:ii for ii, n in enumerate(header)}
+        for line in in_file:
+            params = line.strip().split(',')
+            this = {
+                col: params[header_to_idx[col]]
+                for col in args.column_names
+            }
+            config.append(this)
+    with open(args.dst_path, 'w') as out_file:
+        out_file.write(json.dumps(config, indent=2))
+
+
+if __name__ == "__main__":
+    main()
