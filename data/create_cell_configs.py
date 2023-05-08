34408bd0e19482fb3446763dcc9243f84761697a
danielsf <scott.daniel@alleninstitute.org>
2023-05-08 15:09:34 -0700

    'productionize' config generator

--- data/create_cell_configs.py
+++ data/create_cell_configs.py
@@ -1,31 +1,63 @@
+import argparse
 import json
+import pathlib
 
-raw_path = "raw/extracted_wb10x_cell_info.csv"
 
-config = dict()
+def main():
 
-with open(raw_path, "r") as in_file:
-    header = in_file.readline().strip().split(',')
-    header_to_idx = {
-        n:ii for ii, n in enumerate(header)}
-    for line in in_file:
-        params = line.strip().split(',')
-        if len(params) != len(header):
-            raise RuntimeError("line mismatch")
-        feature = params[header_to_idx['feature_matrix_label']]
-        if feature not in config:
-            config[feature] = []
-        current_config = config[feature]
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--src_path',
+        type=str,
+        default=None,
+        help='path to .csv with cell metadata')
+    parser.add_argument(
+        '--dst_path',
+        type=str,
+        default=None,
+        help='path to file to be written')
+    parser.add_argument(
+        '--column_names',
+        type=str,
+        default=None,
+        nargs='+',
+        help='columns to keep in obs')
+    parser.add_argument(
+        '--subset_column',
+        type=str,
+        default=None,
+        help='column used to denote different subsets')
+    args = parser.parse_args()
 
-        this_cell = {
-            'row_idx': int(params[header_to_idx['source_row_index']]),
-            'cell_label': params[header_to_idx['cell_label']],
-            'cell_barcode': params[header_to_idx['cell_barcode']],
-            'library_label': params[header_to_idx['library_label']],
-            'anatomical_division_label': params[
-                    header_to_idx['anatomical_division_label']]
-        }
-        current_config.append(this_cell)
+    if isinstance(args.column_names, str):
+        column_names = [args.column_names]
+    else:
+        column_names = args.column_names
 
-with open('configs/cell_config.json', 'w') as out_file:
-    out_file.write(json.dumps(config, indent=2))
+    config = dict()
+
+    with open(args.src_path, "r") as in_file:
+        header = in_file.readline().strip().split(',')
+        header_to_idx = {
+            n:ii for ii, n in enumerate(header)}
+        for line in in_file:
+            params = line.strip().split(',')
+            if len(params) != len(header):
+                raise RuntimeError("line mismatch")
+            feature = params[header_to_idx[args.subset_column]]
+            if feature not in config:
+                config[feature] = []
+            current_config = config[feature]
+
+            this_cell = {
+                'row_idx': int(params[header_to_idx['source_row_index']])}
+            for col in args.column_names:
+                this_cell[col] = params[header_to_idx[col]]
+            current_config.append(this_cell)
+
+    with open(args.dst_path, 'w') as out_file:
+        out_file.write(json.dumps(config, indent=2))
+
+
+if __name__ == "__main__":
+    main()
